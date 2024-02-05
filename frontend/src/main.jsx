654ae07aa53714a1e14226326d126d368998d383
okste1234 <okste1234@gmail.com>
2024-02-05 21:00:04 +0100

    fix: working on alias

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,23 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { config } from './config/wagmiConfig.js'
-import { WagmiProvider } from "wagmi"
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+// import { config } from './config/wagmiConfig.js'
+// import { WagmiProvider } from "wagmi"
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+// } from '@tanstack/react-query'
 
 
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </QueryClientProvider>
-  </WagmiProvider>,
+  // <WagmiProvider config={config}>
+  //   <QueryClientProvider client={queryClient}>
+  //     <React.StrictMode>
+  //       <App />
+  //     </React.StrictMode>
+  //   </QueryClientProvider>
+  // </WagmiProvider>
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
